f9c05f892e6e403b5a9a829cf98cb77f7bfcdd00
Francesca Guadagnini <francesca.guadagnini@sysdig.com>
2021-12-01 17:31:30 +0100

    Users + slider

--- Resources/templates/responsive/channel/partials/projects_list.php
+++ Resources/templates/responsive/channel/partials/projects_list.php
@@ -1,4 +1,6 @@
-<div class="slider slider-projects" data-total="<?= (int)$this->total_projects ?>" data-limit="<?= (int)$this->limit ?>">
+<!-- div class="slider slider-projects" data-total="<?= (int)$this->total_projects ?>" data-limit="<?= (int)$this->limit ?>"> -->
+	<div class="" data-total="<?= (int)$this->total_projects ?>" data-limit="<?= (int)$this->limit ?>">
+
     <?php foreach ($this->projects as $project) : ?>
             <div class="item widget-slide">
             <?=    $this->insert('project/widgets/normal', [
